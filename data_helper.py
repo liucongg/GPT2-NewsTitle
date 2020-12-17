6b61674e0b9828e70e700ecfcdd17098e0c51dae
liucongg <lc1584971895@163.com>
2020-12-17 18:03:19 +0800

    update data process code

--- data_helper.py
+++ data_helper.py
@@ -7,22 +7,119 @@
 """
     文件说明：
     数据预处理文件，将数据进行简单的清洗
-    数据来源于两处：（1）新浪微博和（2）NLPCC2017
-    （1）数据链接：https://www.jianshu.com/p/8f52352f0748?tdsourcetag=s_pcqq_aiomsg
-    （2）数据链接：http://tcci.ccf.org.cn/conference/2017/taskdata.php
+    数据来源于新浪微博
+    数据链接：https://www.jianshu.com/p/8f52352f0748?tdsourcetag=s_pcqq_aiomsg
 """
 
 import re
+from functools import partial
+from multiprocessing import Pool, cpu_count
+from tqdm import tqdm
+import json
+import random
 
 
 def clean_weibo_title(title: str):
-    pattern = re.compile(r'(#[1-3])(.*?)(#[1-3])')
-    pattern = ""
-    pattern = re.compile(r'(\[)(.*?)(\])')
+    """
+    对微博数据中的标题内容（待生成）进行清洗
+    Args:
+        title: 标题
 
-    return pattern.sub(r'', pattern.sub(r'', title))
+    Returns:
+
+    """
+    # 去除##中间的文字（一般为微博数据的话题）
+    title = re.sub(r"(#{1,2})(.*?)(#{1,2})", "", title)
+    # 去除[]中间的文字（一般为微博数据中的表情）
+    title = re.sub(r"(\[{1,2})(.*?)(\]{1,2})", "", title)
+    # 合并标题中过多的空格
+    title = re.sub(r"\s+", " ", title)
+    return title
+
+
+def clean_weibo_content(content: str):
+    """
+    对微博数据中的文本内容进行清洗
+    Args:
+        content: 文本
+
+    Returns:
+
+    """
+    # 去除网址
+    content = re.sub(r"(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b", "", content)
+    # 合并正文中过多的空格
+    content = re.sub(r"\s+", " ", content)
+    # 去除\u200b字符
+    content = content.replace("\u200b", "")
+    return content
+
+
+def clean_data(sample):
+    """
+    整体清洗函数，为了方便多线程使用
+    Args:
+        sample: 一个元组，包含正文内容和标题内容
+
+    Returns:
+
+    """
+    (content, title) = sample
+    sample = dict()
+    # 清洗数据
+    sample["title"] = clean_weibo_title(title.strip())
+    sample["content"] = clean_weibo_content(content.strip())
+    return sample
+
+
+def build_news_data(content_path, title_path, train_save_path, test_save_path):
+    """
+    对微博数据进行清洗，构建训练集和测试集
+    Args:
+        content_path: 正文内容文件路径
+        title_path: 标题内容文件路径
+        train_save_path: 训练集文件路径
+        test_save_path: 测试集文件路径
+
+    Returns:
+
+    """
+    # 打开文件，并将其zip成一个文件
+    content_data = open(content_path, "r", encoding="utf-8")
+    title_data = open(title_path, "r", encoding="utf-8")
+    data = zip(content_data.readlines(), title_data.readlines())
+    # 使用多进程处理数据
+    threads = min(8, cpu_count())
+    with Pool(threads) as p:
+        annoate_ = partial(clean_data)
+        data = list(tqdm(p.imap(annoate_, data, chunksize=8),
+                         desc="build data"
+                         )
+                    )
+    # 对数据进行过滤，去除重复数据和正文内容字长小于100的数据
+    data_set = set()
+    data_new = []
+    for d in data:
+        if d["content"] in data_set or len(d["content"]) < 100:
+            continue
+        else:
+            data_set.add(d["content"])
+            data_new.append(d)
+    # 分割数据，构建训练集和测试集
+    random.shuffle(data_new)
+    train_data = data_new[:-3000]
+    test_data = data_new[-3000:]
+    fin = open(train_save_path, "w", encoding="utf-8")
+    fin.write(json.dumps(train_data, indent=4, ensure_ascii=False))
+    fin.close()
+    fin = open(test_save_path, "w", encoding="utf-8")
+    fin.write(json.dumps(test_data, indent=4, ensure_ascii=False))
+    fin.close()
 
 
 if __name__ == '__main__':
-    print(clean_weibo_title("#111111#222[22]22#33333333#"))
-
+    content_path_dir = "F:\\自然语言处理项目\\yunwen_github\\Unilm\\新闻标题数据集\\新闻标题数据集\\train_text.txt"
+    title_path_dir = "F:\\自然语言处理项目\\yunwen_github\\Unilm\\新闻标题数据集\\新闻标题数据集\\train_label.txt"
+    train_save_path_dir = "train_data.json"
+    test_save_path_dir = "test_data.json"
+    build_news_data(content_path_dir, title_path_dir, train_save_path_dir, test_save_path_dir)
--- data_set.py
+++ data_set.py
@@ -6,5 +6,65 @@
 # @time: 2020/12/16 16:25
 """
     文件说明：
-    
+    数据类文件，定义模型所需的数据类，方便模型训练使用
 """
+
+import torch
+import json
+import os
+from tqdm import tqdm
+from torch.utils.data import Dataset
+
+
+class GPT2NewsTitleDataSet(Dataset):
+    def __init__(self, tokenizer, max_len, data_dir, data_set_name, path_file=None, is_overwrite=False):
+        self.tokenizer = tokenizer
+        self.content_id = self.tokenizer.convert_tokens_to_ids("[content]")
+        self.title_id = self.tokenizer.convert_tokens_to_ids("[title]")
+        self.space_id = self.tokenizer.convert_tokens_to_ids("[space]")
+        self.max_len = max_len
+        cached_feature_file = os.path.join(data_dir, "cached_{}_{}".format(data_set_name, max_len))
+        if os.path.exists(cached_feature_file) and not is_overwrite:
+            self.data_set = torch.load(cached_feature_file)["data_set"]
+        else:
+            self.data_set = self.load_data(path_file)
+            torch.save({"data_set": self.data_set}, cached_feature_file)
+
+    def load_data(self, path_file):
+        self.data_set = []
+        with open(path_file, "r", encoding="utf-8") as fh:
+            data = json.load(fh)
+            for idx, sample in enumerate(tqdm(data, desc="iter", disable=False)):
+                input_ids, token_type_ids = self.convert_feature(sample)
+                self.data_set.append({"input_ids": input_ids, "token_type_ids": token_type_ids})
+        return self.data_set
+
+    def convert_feature(self, sample):
+        input_ids = []
+        token_type_ids = []
+        content_tokens = self.tokenizer.tokenize(sample["content"])
+        title_tokens = self.tokenizer.tokenize(sample["title"].replace(" ", "[space]"))
+        if len(content_tokens) > self.max_len - len(title_tokens) - 3:
+            content_tokens = content_tokens[:self.max_len - len(title_tokens) - 3]
+        input_ids.append(self.tokenizer.cls_token_id)
+        token_type_ids.append(self.content_id)
+        input_ids.extend(self.tokenizer.convert_tokens_to_ids(content_tokens))
+        token_type_ids.extend([self.content_id] * len(content_tokens))
+        input_ids.append(self.tokenizer.sep_token_id)
+        token_type_ids.append(self.content_id)
+        input_ids.extend(self.tokenizer.convert_tokens_to_ids(title_tokens))
+        token_type_ids.extend([self.title_id] * len(title_tokens))
+        input_ids.append(self.tokenizer.sep_token_id)
+        token_type_ids.append(self.title_id)
+        return input_ids, token_type_ids
+
+    def __len__(self):
+        return len(self.data_set)
+
+    def __getitem__(self, item):
+        instance = self.data_set[item]
+        return instance
+
+
+def collate_func(batch_data):
+    return {}
